52a4448b33a6e8e02c2ee1c17730f670d6331228
Andrii Fedorenko <andrii.fedorenko@onedata.de>
2024-04-23 18:10:43 +0200

    added bigration api interface

---
+++ interfaces/studio-api-interface/src/apis/index.ts
@@ -0,0 +1,25 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AccountControllerApi';
+export * from './AccountMemberControllerApi';
+export * from './AccountStatisticsControllerApi';
+export * from './ActionControllerApi';
+export * from './ActionPositionControllerApi';
+export * from './EditorControllerApi';
+export * from './EditorSettingsControllerApi';
+export * from './ImportExportControllerApi';
+export * from './InstanceControllerApi';
+export * from './IntegrationControllerApi';
+export * from './IntegrationValidatorControllerApi';
+export * from './ManualWebhookTriggerControllerApi';
+export * from './NotificationControllerApi';
+export * from './ProjectAccessControllerApi';
+export * from './ProjectControllerApi';
+export * from './QuotaControllerApi';
+export * from './StatisticsControllerApi';
+export * from './WebhookTriggerControllerApi';
+export * from './WorkflowControllerApi';
+export * from './WorkflowEngineControllerApi';
+export * from './WorkflowEngineServiceApiInstanceControllerApi';
+export * from './WorkflowTemplateControllerApi';
+export * from './WorkspaceControllerApi';
